3320f9f6246de0c01bb7f34e267e2956bf589e03
cktricky <enduser@cktrickys-LS-MBP.local>
2013-01-31 09:40:09 -0500

    just making sure everything is up-to-date

--- decoder_ring.rb
+++ decoder_ring.rb
@@ -3,6 +3,8 @@
 require 'java'
 require 'base64'
 
+import java.awt.Dimension
+import java.awt.FlowLayout
 import javax.swing.JFrame
 import javax.swing.JPanel
 import java.awt.Dimension
@@ -13,6 +15,49 @@ import javax.swing.GroupLayout
 import javax.swing.JScrollPane
 import javax.swing.JOptionPane
 import javax.swing.JComboBox;
+
+class EncodeButtonAndCbPanel < JPanel
+  
+  def initialize
+    super(FlowLayout.new(FlowLayout::RIGHT))
+    initUI
+  end
+  
+  def initUI
+     @jc1 = JComboBox.new
+     ["Marshal", "Base64", "Rails Session Cookie"].each {|x| @jc1.addItem(x)}
+     
+      @encode_button = JButton.new("encode")
+      @decode_button = JButton.new("decode")
+     # add(@jc1)
+     # add(@encode_button)
+     # add(@decode_button)
+   
+      layout = GroupLayout.new self
+      self.setLayout layout
+      
+      layout.setAutoCreateGaps true
+      layout.setAutoCreateContainerGaps true
+      
+      sh1 = layout.createSequentialGroup
+      sv1 = layout.createSequentialGroup
+      p1 = layout.createParallelGroup
+      
+      layout.setHorizontalGroup sh1
+      layout.setVerticalGroup sv1
+      
+      p1.addComponent(@jc1)
+      p1.addComponent(@encode_button)
+      p1.addComponent(@decode_button)
+      sh1.addGroup(p1)
+      
+      sv1.addComponent(@jc1)
+      sv1.addComponent(@encode_button)
+      sv1.addComponent(@decode_button)
+        
+  end  
+    
+end
 
 class MessagePanel < JPanel
   
@@ -54,8 +99,6 @@ class MessagePanel < JPanel
     sh1.addComponent(@sp1)
     sh1.addComponent(@exit_button)
     
-  
-    
   end
   
 end
@@ -69,16 +112,15 @@ class DecoderPanel < JPanel
   
   def initUI
     
-    @jc1 = JComboBox.new
-    @jc1.addItem("Marshal")
-    @jc1.addItem("Base64")
-    @jc1.addItem("Rails Session Cookie")
-    
+    @ep = EncodeButtonAndCbPanel.new
     
     @jt1 = JTextArea.new
     @jt2 = JTextArea.new
+    @jt1.setPreferredSize(Dimension.new(700,200))
+    @jt2.setPreferredSize(Dimension.new(700,200))
     @js1 = JScrollPane.new(@jt1)
     @js2 = JScrollPane.new(@jt2)
+    
     
     layout = GroupLayout.new self
     self.setLayout layout
@@ -94,26 +136,24 @@ class DecoderPanel < JPanel
     sh3 = layout.createSequentialGroup
     p1 = layout.createParallelGroup
     p2 = layout.createParallelGroup
+    p3 = layout.createParallelGroup
     
     layout.setHorizontalGroup sh1
     layout.setVerticalGroup sv1
 
 
     p1.addComponent(@js1)
-    p1.addComponent(@jc1)
+    p1.addComponent(@ep)
     sv2.addGroup(p1)
-    sv3.addComponent(@js2)
+    sv2.addComponent(@js2)
     sv1.addGroup(sv2)
-    sv1.addGroup(sv3)
     
     p2.addComponent(@js1)
     p2.addComponent(@js2)
     sh2.addGroup(p2)
-    sh3.addComponent(@jc1)
+    sh2.addComponent(@ep)
     sh1.addGroup(sh2)
-    sh1.addGroup(sh3)
-    
-    
+
   end
   
 end
